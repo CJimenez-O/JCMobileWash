6b9f880dea0014134214f6511a8db6966ccf6db4
legazy33 <chris.jimenez50@yahoo.com>
2020-09-08 23:09:19 -0700

    deleted contact js

--- js/app.js
+++ js/app.js
@@ -13,26 +13,3 @@ document.querySelector(".nav-links").addEventListener("click", function(e){
     console.log(e.target);
 });
 
-
-// sending email
-$(document).ready(function(){
-    $('.submit').click(function(e){
-
-
-        let email = $('.email').val();
-        let name = $('.name').val();
-        let phone = $('.phone').val();
-        let day = $('.day').val();
-        let time = $('.time').val();
-        let statusElm = $('.status');
-        statusElm.empty();
-
-        if(email.length > 5 && email.includes("@") && email.includes(".")){
-            e.preventDefault();
-            statusElm.append('<div>Email is Valid</div>');
-        } else{
-            e.preventDefault();
-            statusElm.append('<div>Email is not Valid</div>');
-        }
-    })
-})
