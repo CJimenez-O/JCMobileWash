35cfdc810e86a5b5387c4690558c740c3dbc4f00
legazy33 <chris.jimenez50@yahoo.com>
2020-09-11 21:10:42 -0700

    updated homepage optimized and better responcive

--- js/app.js
+++ js/app.js
@@ -5,11 +5,29 @@ console.log(document.querySelector(".og-image").style.width );
 });
 
 
-// quick fix for nav bar to go away after clicking link
+nav = document.querySelector(".nav-links");
 document.querySelector(".nav-links").addEventListener("click", function(e){
     let nav = document.querySelector("nav");
-    nav.remove();
-    location.reload();
-    console.log(e.target);
+    let burger = nav.childNodes[3];
+    burger.removeAttribute("id");
+    //location.reload();
+    //console.log(e.target);
+    console.log(burger);
+
 });
 
+const menu = document.querySelector(".menu-btn");
+
+menu.addEventListener("click", function(e){
+    let nav2 = document.querySelector("nav");  
+
+    let burger = nav2.childNodes[3];
+    burger.setAttribute('id', "click");
+})
+
+
+
+const btn = document.querySelector(".button")
+btn.addEventListener("click", function(e){
+    location.href='#contact';
+})
